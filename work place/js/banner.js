c124dd1291f63d8b0ed0cb57140ec8b9aee80efd
Artem Slovachevs'kyi <aslov@macmini1465.ddns.softservecom.com>
2018-12-05 16:53:25 +0200

    slider fix

--- work place/js/banner.js
+++ work place/js/banner.js
@@ -9,34 +9,13 @@
 		slider.setup('masterslider-index' , {
 			width:1366,
 			height:768,
-			space:10,
+			space:0,
+            count: 3,
+            view:"basic",
 			layout:'fullscreen',
-			fullscreenMargin:0,
-			loop:true,
-			speed:5,
-            preload:0,
+            speed:6,
 			autoplay: true
 		});
-	// End jQuery Banner Index 
-
-    // jQuery Banner Layer     
-     var slider = new MasterSlider();
-    
-    // adds Arrows navigation control to the slider.
-    slider.control('arrows');
-    slider.control('timebar' , {insertTo:'#masterslider'});
-    slider.control('bullets');
-    
-     slider.setup('masterslider' , {
-    	 width:1400,    // slider standard width
-    	 height:768,   // slider standard height
-    	 space:1,
-    	 layout:'fullscreen',
-    	 loop:true,
-		 fullscreenMargin:0,
-    	 autoplay:true
-		 
-    });
-	// End jQuery Banner Layer 	
+	// End jQuery Banner Index
 })(jQuery);
 	
